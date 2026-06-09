76c136c0a9e78a824d5c4c0c979fc619b1649a6f
Charles-ccc <liu930505@qq.com>
2026-06-09 14:20:50 +0800

    fix: 从头再来

--- apps/h5-staff/config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    NODE_ENV: '"development"',
-    TARO_APP_USE_FRONTEND_MOCK: '"true"',
-  },
-  defineConstants: {},
-  mini: {},
-  h5: {},
-}
--- apps/h5-staff/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare const process: {
-  env: {
-    NODE_ENV?: string;
-    TARO_APP_API_URL?: string;
-    TARO_APP_SHARE_URL?: string;
-    TARO_APP_USE_FRONTEND_MOCK?: string;
-    [key: string]: string | undefined;
-  };
-};
